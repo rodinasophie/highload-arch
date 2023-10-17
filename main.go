f38475f2dd74e2f3022b659c262fa3bc1a6745d8
Sofia Rodina <rodinasophie@gmail.com>
2023-10-17 14:53:16 +0300

    Update main.go

--- main.go
+++ main.go
@@ -8,41 +8,6 @@ import (
 	"net/http"
 )
 
-/*func RunReq() error {
-	db, err := pgx.Connect(context.Background(), "host=localhost port=5432 user=admin_user password=1111 dbname=social_net sslmode=disable")
-	if err != nil {
-		log.Fatal(err)
-	}
-	regexMap := make(map[string]string)
-	regexMap["first_name"] = string('\'') + string(rune('У')) + string('%') + string('\'')
-	regexMap["second_name"] = string('\'') + string(rune('Р')) + string('%') + string('\'')
-	regexFilter := ``
-	var regexMapArray []interface{}
-	id := 1
-	for key, value := range regexMap {
-		newFilter := fmt.Sprintf(`%s LIKE %s`, key, value)
-		if regexFilter != `` {
-			regexFilter += ` and ` + newFilter
-		} else {
-			regexFilter = newFilter
-			id += 1
-		}
-		regexMapArray = append(regexMapArray, value)
-	}
-	regexFilter = `SELECT * FROM users WHERE ` + regexFilter + ` ORDER BY id`
-	fmt.Println(regexFilter)
-	start := time.Now()
-	rows, err := db.Query(context.Background(), regexFilter)
-	defer rows.Close()
-	if err != nil {
-		log.Fatal(err)
-		return err
-	}
-	elapsed := time.Since(start)
-	fmt.Printf("Execution of Select: %v\n", elapsed)
-	return nil
-}*/
-
 func main() {
 	config.Load("local-config.yaml")
 	storage.CreateConnectionPool()
